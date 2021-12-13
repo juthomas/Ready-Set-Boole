16714e08ed3629fc57dd91e1293a53043ad31b9b
juthomas <juthomas@student.42.fr>
2021-12-13 15:01:09 +0100

    [CLN] Suppr tabs and returns

--- ex01/multiplier.c
+++ ex01/multiplier.c
@@ -4,17 +4,17 @@
 
 void printBits(size_t const size, void const * const ptr)
 {
-    unsigned char *b = (unsigned char*) ptr;
-    unsigned char byte;
-    int i, j;
-    
-    for (i = size-1; i >= 0; i--) {
-        for (j = 7; j >= 0; j--) {
-            byte = (b[i] >> j) & 1;
-            printf("%u", byte);
-        }
-    }
-    puts("");
+	unsigned char *b = (unsigned char*) ptr;
+	unsigned char byte;
+	int i, j;
+	
+	for (i = size-1; i >= 0; i--) {
+		for (j = 7; j >= 0; j--) {
+			byte = (b[i] >> j) & 1;
+			printf("%u", byte);
+		}
+	}
+	puts("");
 }
 
 uint32_t adder(uint32_t a, uint32_t b)
