d3b452d236fdfc035dab60626b9943295a89f3a6
juthomas <juthomas@student.42.fr>
2021-12-19 19:11:03 +0100

    [ADD] ex08

---
+++ ex08/main.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <vector>
+#include <array>
+
+std::vector<std::vector<int32_t>> powerset(std::vector<int32_t> set);
+
+// void printBits(size_t const size, void const * const ptr);
+
+void tokenize(std::string const &str, const char delim,
+            std::vector<int32_t> &out)
+{
+    size_t start;
+    size_t end = 0;
+ 
+    while ((start = str.find_first_not_of(delim, end)) != std::string::npos)
+    {
+        end = str.find(delim, start);
+        out.push_back( atoi(str.substr(start, end - start).c_str()));
+    }
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		std::string str(argv[1]);
+	    std::vector<int32_t> out;
+		tokenize(str, ' ', out);
+
+		printf("input : \n");
+		printf("[");
+		for (int32_t &s: out) {
+			printf("%d, ", s);
+    	}
+		printf("]\n\n");
+
+
+		std::vector<std::vector<int32_t>> powerset_output;
+		powerset_output = powerset(out);
+		printf("output : \n");
+		printf("[\n");
+
+		for (std::vector<int32_t> &setofnu: powerset_output) {
+				printf(" [");
+				for (int32_t &nu: setofnu) {
+				printf("%d,", nu);
+			}
+			printf("]\n");
+		}
+		printf("]\n");
+
+
+	}
+}
---
+++ ex08/powerset.cpp
@@ -0,0 +1,62 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <string>
+#include <array>
+// using std;
+
+void printBits(size_t const size, void const * const ptr)
+{
+	unsigned char *b = (unsigned char*) ptr;
+	unsigned char byte;
+	int i, j;
+	
+	for (i = size-1; i >= 0; i--) {
+		for (j = 7; j >= 0; j--) {
+			byte = (b[i] >> j) & 1;
+			printf("%u", byte);
+		}
+	}
+	puts("");
+}
+
+std::vector<std::vector<int32_t>> powerset(std::vector<int32_t> set)
+{
+	std::vector<std::vector<int32_t>> return_value;
+	// uint32_t xor_result;
+	// uint32_t and_result;
+
+	// while (a & b)
+	// {
+	// 	xor_result = a ^ b;
+	// 	and_result = (a & b) << 1;
+	// 	a = xor_result;
+	// 	b = and_result;
+	// }
+	uint64_t max_iterations = 1 << set.size();
+	// printf("max iterations :%ld\n", max_iterations);
+
+	for (uint64_t current_iteration = 0; current_iteration < max_iterations; current_iteration++)
+	{
+		std::vector<int32_t> new_row;
+		for (uint64_t i = 0; i < set.size(); i++)
+		{
+				// printf("cc %d\n", 1 << i);
+				// printf("cd %d\n", max_iterations);
+
+			if (1 << i & current_iteration)
+			{
+				// printf("cc\n");
+				new_row.push_back(set[i]);
+			}
+		}
+		return_value.push_back(new_row);
+	}
+
+	// return_value.push_back(std::vector<int32_t>());
+
+
+
+	return (return_value);
+}
